f82475df1bb29123d5dfe53a00cb115fe73ea1f7
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-12 12:42:59 -0800

    Problem six complete.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -20,7 +20,7 @@ namespace DatabaseFirstLINQ
             ProblemThree();
             ProblemFour();
             ProblemFive();
-            //ProblemSix();
+            ProblemSix();
             //ProblemSeven();
             //ProblemEight();
             //ProblemNine();
@@ -108,12 +108,17 @@ namespace DatabaseFirstLINQ
 
         }
 
-        //        private void ProblemSix()
-        //        {
-        //            // Write a LINQ query that gets all of the users who registered AFTER 2016 and BEFORE 2018
-        //            // Then print each user's email and registration date to the console.
+        private void ProblemSix()
+        {
+            // Write a LINQ query that gets all of the users who registered AFTER 2016 and BEFORE 2018
+            var users = _context.Users.Where(x => x.RegistrationDate > new DateTime(2016, 1, 1) && x.RegistrationDate < new DateTime(2018, 1, 1));
+            // Then print each user's email and registration date to the console.
+            foreach( User user in users)
+            {
+                Console.WriteLine($"Problem six:email: {user.Email} date:{user.RegistrationDate} password: {user.Password}");
+            }
 
-        //        }
+        }
 
         //        // <><><><><><><><> R Actions (Read) with Foreign Keys <><><><><><><><><>
 

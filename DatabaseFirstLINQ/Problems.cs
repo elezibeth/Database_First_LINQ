a37a137457dd59e365519f0fd57cad0d1f624fe5
elezibeth <elezibethmorgan@my.unt.edu>
2021-03-15 09:52:37 -0700

    Problem 12 complete repush.

--- DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/Problems.cs
@@ -174,11 +174,11 @@ namespace DatabaseFirstLINQ
         private void ProblemEleven()
         {
             //Create a new User object and add that user to the Users table using LINQ.
-           User newUser = new User()
-           {
-               Email = "david@gmail.com",
-               Password = "DavidsPass123"
-           };
+            User newUser = new User()
+            {
+                Email = "david@gmail.com",
+                Password = "DavidsPass123"
+            };
             _context.Users.Add(newUser);
             _context.SaveChanges();
         }
@@ -197,128 +197,129 @@ namespace DatabaseFirstLINQ
             _context.Products.Add(product);
             _context.SaveChanges();
 
-                
-      
+
+
         }
-        
-        
-
-    }
 
 
 
 
 
-//private void ProblemThirteen()
-//{
-//    // Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
-//    var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
-//    var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
-//    UserRole newUserRole = new UserRole()
-//    {
-//        UserId = userId,
-//        RoleId = roleId
-//    };
-//    _context.UserRoles.Add(newUserRole);
-//    _context.SaveChanges();
-//}
-//}
 
-        //        private void ProblemFourteen()
-        //        {
-        //            // Add the product you create to the user we created in the ShoppingCart junction table using LINQ.
 
-        //        }
 
-        //        // <><> U Actions (Update) <><>
 
-        //        private void ProblemFifteen()
-        //        {
-        //            // Update the email of the user we created to "mike@gmail.com"
-        //            var user = _context.Users.Where(u => u.Email == "david@gmail.com").SingleOrDefault();
-        //            user.Email = "mike@gmail.com";
-        //            _context.Users.Update(user);
-        //            _context.SaveChanges();
-        //        }
+        private void ProblemThirteen()
+        {
+            // Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
+            var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
+            var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
+            UserRole newUserRole = new UserRole()
+            {
+                UserId = userId,
+                RoleId = roleId
+            };
+            _context.UserRoles.Add(newUserRole);
+            _context.SaveChanges();
+        }
 
-        //        private void ProblemSixteen()
-        //        {
-        //            // Update the price of the product you created to something different using LINQ.
 
-        //        }
 
-        //        private void ProblemSeventeen()
-        //        {
-        //            // Change the role of the user we created to "Employee"
-        //            // HINT: You need to delete the existing role relationship and then create a new UserRole object and add it to the UserRoles table
-        //            // See problem eighteen as an example of removing a role relationship
-        //            var userRole = _context.UserRoles.Where(ur => ur.User.Email == "mike@gmail.com").SingleOrDefault();
-        //            _context.UserRoles.Remove(userRole);
-        //            UserRole newUserRole = new UserRole()
-        //            {
-        //                UserId = _context.Users.Where(u => u.Email == "mike@gmail.com").Select(u => u.Id).SingleOrDefault(),
-        //                RoleId = _context.Roles.Where(r => r.RoleName == "Employee").Select(r => r.Id).SingleOrDefault()
-        //            };
-        //            _context.UserRoles.Add(newUserRole);
-        //            _context.SaveChanges();
-        //        }
+        private void ProblemFourteen()
+        {
+                    // Add the product you create to the user we created in the ShoppingCart junction table using LINQ.
 
-        //        // <><> D Actions (Delete) <><>
+         }
 
-        //        private void ProblemEighteen()
-        //        {
-        //            // Delete the role relationship from the user who has the email "oda@gmail.com" using LINQ.
+    //        // <><> U Actions (Update) <><>
 
-        //        }
+    //        private void ProblemFifteen()
+    //        {
+    //            // Update the email of the user we created to "mike@gmail.com"
+    //            var user = _context.Users.Where(u => u.Email == "david@gmail.com").SingleOrDefault();
+    //            user.Email = "mike@gmail.com";
+    //            _context.Users.Update(user);
+    //            _context.SaveChanges();
+    //        }
 
-        //        private void ProblemNineteen()
-        //        {
-        //            // Delete all of the product relationships to the user with the email "oda@gmail.com" in the ShoppingCart table using LINQ.
-        //            // HINT: Loop
-        //            var shoppingCartProducts = _context.ShoppingCarts.Where(sc => sc.User.Email == "oda@gmail.com");
-        //            foreach (ShoppingCart userProductRelationship in shoppingCartProducts)
-        //            {
-        //                _context.ShoppingCarts.Remove(userProductRelationship);
-        //            }
-        //            _context.SaveChanges();
-        //        }
+    //        private void ProblemSixteen()
+    //        {
+    //            // Update the price of the product you created to something different using LINQ.
 
-        //        private void ProblemTwenty()
-        //        {
-        //            // Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
+    //        }
 
-        //        }
+    //        private void ProblemSeventeen()
+    //        {
+    //            // Change the role of the user we created to "Employee"
+    //            // HINT: You need to delete the existing role relationship and then create a new UserRole object and add it to the UserRoles table
+    //            // See problem eighteen as an example of removing a role relationship
+    //            var userRole = _context.UserRoles.Where(ur => ur.User.Email == "mike@gmail.com").SingleOrDefault();
+    //            _context.UserRoles.Remove(userRole);
+    //            UserRole newUserRole = new UserRole()
+    //            {
+    //                UserId = _context.Users.Where(u => u.Email == "mike@gmail.com").Select(u => u.Id).SingleOrDefault(),
+    //                RoleId = _context.Roles.Where(r => r.RoleName == "Employee").Select(r => r.Id).SingleOrDefault()
+    //            };
+    //            _context.UserRoles.Add(newUserRole);
+    //            _context.SaveChanges();
+    //        }
 
-        //        // <><><><><><><><> BONUS PROBLEMS <><><><><><><><><>
+    //        // <><> D Actions (Delete) <><>
 
-        //        private void BonusOne()
-        //        {
-        //            // Prompt the user to enter in an email and password through the console.
-        //            // Take the email and password and check if the there is a person that matches that combination.
-        //            // Print "Signed In!" to the console if they exists and the values match otherwise print "Invalid Email or Password.".
-        //        }
+    //        private void ProblemEighteen()
+    //        {
+    //            // Delete the role relationship from the user who has the email "oda@gmail.com" using LINQ.
 
-        //        private void BonusTwo()
-        //        {
-        //            // Write a query that finds the total of every users shopping cart products using LINQ.
-        //            // Display the total of each users shopping cart as well as the total of the toals to the console.
-        //        }
+    //        }
 
-        //        // BIG ONE
-        //        private void BonusThree()
-        //        {
-        //            // 1. Create functionality for a user to sign in via the console
-        //            // 2. If the user succesfully signs in
-        //                // a. Give them a menu where they perform the following actions within the console
-        //                    // View the products in their shopping cart
-        //                    // View all products in the Products table
-        //                    // Add a product to the shopping cart (incrementing quantity if that product is already in their shopping cart)
-        //                    // Remove a product from their shopping cart
-        //            // 3. If the user does not succesfully sing in
-        //                // a. Display "Invalid Email or Password"
-        //                // b. Re-prompt the user for credentials
+    //        private void ProblemNineteen()
+    //        {
+    //            // Delete all of the product relationships to the user with the email "oda@gmail.com" in the ShoppingCart table using LINQ.
+    //            // HINT: Loop
+    //            var shoppingCartProducts = _context.ShoppingCarts.Where(sc => sc.User.Email == "oda@gmail.com");
+    //            foreach (ShoppingCart userProductRelationship in shoppingCartProducts)
+    //            {
+    //                _context.ShoppingCarts.Remove(userProductRelationship);
+    //            }
+    //            _context.SaveChanges();
+    //        }
 
-        //        }
+    //        private void ProblemTwenty()
+    //        {
+    //            // Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
 
-    }
+    //        }
+
+    //        // <><><><><><><><> BONUS PROBLEMS <><><><><><><><><>
+
+    //        private void BonusOne()
+    //        {
+    //            // Prompt the user to enter in an email and password through the console.
+    //            // Take the email and password and check if the there is a person that matches that combination.
+    //            // Print "Signed In!" to the console if they exists and the values match otherwise print "Invalid Email or Password.".
+    //        }
+
+    //        private void BonusTwo()
+    //        {
+    //            // Write a query that finds the total of every users shopping cart products using LINQ.
+    //            // Display the total of each users shopping cart as well as the total of the toals to the console.
+    //        }
+
+    //        // BIG ONE
+    //        private void BonusThree()
+    //        {
+    //            // 1. Create functionality for a user to sign in via the console
+    //            // 2. If the user succesfully signs in
+    //                // a. Give them a menu where they perform the following actions within the console
+    //                    // View the products in their shopping cart
+    //                    // View all products in the Products table
+    //                    // Add a product to the shopping cart (incrementing quantity if that product is already in their shopping cart)
+    //                    // Remove a product from their shopping cart
+    //            // 3. If the user does not succesfully sing in
+    //                // a. Display "Invalid Email or Password"
+    //                // b. Re-prompt the user for credentials
+
+    //        }
+
+}
 }
